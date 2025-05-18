25407a612de0d5875787a4b13ccd681b6042dd6a
Fahimfaisaltalha <faisal15-4721@diu.edu.bd>
2025-05-18 21:00:13 +0600

    first commit

---
+++ todo.js
@@ -0,0 +1,13 @@
+let todos=[
+    {title: "learn JS",done: false},
+    {title: "learn HTML",done: true},
+    {title: "Do Homework",done: false},
+    {title: "Understanding the array",done: true},
+]
+
+todos.forEach((todo)=>{
+    console.log(`Title: ${todo.title} is done : ${todo.done}`);
+
+})
+
+    
